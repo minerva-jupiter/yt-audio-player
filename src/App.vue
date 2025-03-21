ad7d3939dc0ab7e60ba606d0f47dc3d2fcd60025
minerva-jupiter <ryouturn@gmail.com>
2025-03-21 11:01:23 +0900

    add some albums to json

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@ import PlayUI from './components/PlayUI.vue';
   <div class="container">
     <nav class="navbar navbar-expand-lg">
       <router-link to="/" class="navbar-brand">yt-audio-player</router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
-            <router-link to="/" class="nav-link">Home</router-link>
+            <router-link to="/" class="nav-link" aria-current="page">Home</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/about" class="nav-link">About</router-link>
--- src/components/ArtistsList.vue
+++ src/components/ArtistsList.vue
@@ -4,9 +4,11 @@ import albums from '../assets/albums.json';
 
 <template>
     <h2>ArtistList</h2>
-    <div class="card m-3" style="width: 18rem;" v-for="(artists, index) in albums" @click="() => $router.push(`/artist/${index}`)">
-        <div class="card-body">
-            <h5 class="card-title">{{ artists.AlbumArtist }}</h5>
+    <div class="card-deck">
+        <div class="card m-3" style="width: 18rem;" v-for="(artists, index) in albums" @click="() => $router.push(`/artist/${index}`)">
+            <div class="card-body">
+                <h5 class="card-title">{{ artists.AlbumArtist }}</h5>
+            </div>
         </div>
     </div>
 </template>
