16e3b7bd30a12f2a44bdd52db871fd0d6db63eec
minerva-jupiter <ryouturn@gmail.com>
2025-03-22 20:32:53 +0900

    とりあえずcommit

--- src/components/PlayUI.vue
+++ src/components/PlayUI.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="fixed-bottom">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393"/>
+        <div class="d-flex justify-content-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-stop-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5z"/>
             </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-chevron-bar-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M4.146 3.646a.5.5 0 0 0 0 .708L7.793 8l-3.647 3.646a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708 0M11.5 1a.5.5 0 0 1 .5.5v13a.5.5 0 0 1-1 0v-13a.5.5 0 0 1 .5-.5"/>
+            </svg>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+</script>
---
+++ src/player.ts
@@ -0,0 +1,36 @@
+import { ref } from 'vue';
+
+ export interface Queue {
+     video_id: string;
+     title: string;
+ };
+ 
+ let queue: Queue[] = [];
+ const isPlaying = ref(false);
+ 
+ function add_to_queue(list: Queue[]): void {
+     console.log("add_to_queue was called");
+     for (let i = 0; i < list.length; i++) {
+         queue.push(list[i]);
+     };
+     console.log(queue);
+ }
+ 
+ function remove_queue(): void {
+     console.log("remove_queue was called");
+     queue = [];
+ }
+ 
+ function next_song(): void {
+     console.log("next_song was called");
+     queue.shift();
+     console.log(queue);
+ }
+ 
+ function play(): void {
+     console.log("play_pause was called");
+     isPlaying.value = !isPlaying.value;
+ }
+
+ 
+ export default {add_to_queue, remove_queue, next_song, play, isPlaying};
